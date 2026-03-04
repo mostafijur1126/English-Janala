f07021ee2aa21f0e108af1bf84e4f3ea4c03ab50
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-03-05 00:10:11 +0600

    fix essue in banner image

--- test.js
+++
@@ -1,7 +0,0 @@
-
-const createElements = (arr)=> {
-    const htmlelements = arr.map(el => `<span class ="btn" ${el}</span>`);
-    console.log(htmlelements.join(" "))
-}
-const synonyms = ["hello","hi", "bye"];
-createElements(synonyms);
